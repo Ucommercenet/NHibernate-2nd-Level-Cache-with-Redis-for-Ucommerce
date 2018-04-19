b6a8cbd3fc0c7382628dcebc919d7f8d398f4865
Morten Lyhr <morten.lyhr@ucommerce.net>
2018-04-19 12:34:02 +0200

    Solution

---
+++ RedisForUcommerce/RedisSessionProvider.cs
@@ -0,0 +1,32 @@
+﻿using NHibernate;
+using NHibernate.Caches.Redis;
+using StackExchange.Redis;
+using UCommerce.EntitiesV2;
+
+namespace RedisForUcommerce
+{
+    public class RedisSessionProvider : SessionProvider
+    {
+        protected override ISessionFactory CreateSessionFactory(bool enableCache, string cacheProvider)
+        {
+            if (enableCache)
+            {
+                // Or use your IoC container to wire this up.
+                var connectionMultiplexer = ConnectionMultiplexer.Connect("localhost:6379");
+                RedisCacheProvider.SetConnectionMultiplexer(connectionMultiplexer);
+
+                var options = new RedisCacheProviderOptions()
+                {
+                    Serializer = new NhJsonCacheSerializer(),
+                };
+                RedisCacheProvider.SetOptions(options);
+
+                
+            }
+            
+            var sessionFactory = base.CreateSessionFactory(enableCache, cacheProvider);
+
+            return sessionFactory;
+        }
+    }
+}
